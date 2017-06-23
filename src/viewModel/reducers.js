a4cbb9d31e6342cfd914cdffdddfa21c106e52e1
Jess Champion <jess@silverstripe.com>
2017-06-23 16:59:28 +1200

    Update team members in indervidual view

--- src/viewModel/reducers.js
+++ src/viewModel/reducers.js
@@ -3,14 +3,26 @@ import * as R from 'ramda';
 
 const addPlainSprite = R.map(R.assoc('sprite', 'plain'));
 
+const getTeamFromAtom = R.path(['person', 'team']);
+
+const isInTeam = (team, atom) => {
+    return R.compose(R.contains(team), getTeamFromAtom)(atom);
+};
+
 export default function viewModelReducer(state = {atoms: []}, action) {
     console.log('action: ');
     console.log(action);
     switch (action.type) {
         case CREATE_INDIVIDUAL_VM: {
+            const targetID = parseInt(action.target);
+            const targetTeam = R.compose(R.head, R.prop('team'), R.find(R.propEq('id', targetID)))(action.people);
+            console.log(targetTeam);
             return {atoms: R.map((atom) => {
-                if(atom.id === parseInt(action.target)){
+                if(atom.id === targetID){
                     return R.assoc('sprite', 'green')(atom);
+                }
+                if(R.contains(targetTeam)(atom.team)){
+                    return R.assoc('sprite', 'blue')(atom);
                 }
                 return R.assoc('sprite', 'plain')(atom);
             })(action.people)};
